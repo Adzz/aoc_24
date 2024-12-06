40643cb2a14d81159d18a35b5031ebd7d76085e9
Adam Lancaster <lancaster28@hotmail.com>
2024-12-06 02:49:14 -0600

    Day 4 Part 1

--- lib/aoc24.ex
+++ lib/aoc24.ex
@@ -304,4 +304,165 @@ defmodule Aoc24 do
   def parse_instructions(<<_::binary-size(1), rest::binary>>, index, _stack, total) do
     parse_instructions(rest, index + 1, [], total)
   end
+
+  @doc """
+  This is like a word search but on steroids words can appear in any order and direction
+  and can overlap. The approach we took was to create a binary of the rows, columns then
+  each diagonal. Once we have the binary in whichever shape we want we check for XMAS and
+  SAMX to handle both directions.
+
+  For rows we do nothing as the puzzle input is already row major. To get columns we iterate
+  over the binary in a way that lands us on the correct next character for the column.
+
+  We do a similar thing for diagonals, but first generate the list of indexes into the
+  square grid that would describe each row if it were a row of diagonals. Then we extract
+  the characters from the original binary by converting those x/y coords into character byte
+  positions and using :binary_part.
+
+  For diagonals you can trim the first and last 3 rows because you can't make a long enough
+  diagonal for there to be a match. In reality this is a very tiny optimization.
+
+  Example problem with an answer of 18:
+
+      MMMSXXMASM
+      MSAMXMSMSA
+      AMXSXMAAMM
+      MSAMASMSMX
+      XMASAMXAMM
+      XXAMMXXAMA
+      SMSMSASXSS
+      SAXAMASAAA
+      MAMMMXMMMM
+      MXMXAXMASX
+
+  2543 is the answer
+  """
+  def day4_1() do
+    grid = "./day_4_input.txt" |> File.read!()
+
+    line_length = line_length(grid, 0)
+    row_count = check_line(grid, 0)
+    ne_count = north_east_diagonal(grid, line_length)
+    se_count = south_east_diagonal(grid, line_length)
+    column_count = column_count(grid, line_length)
+    row_count + ne_count + se_count + column_count
+  end
+
+  @samx "SAMX"
+  @xmas "XMAS"
+  @new_line "\n"
+  defp check_line(<<>>, hits), do: hits
+
+  defp check_line(<<@samx, _::binary>> = line, hits) do
+    <<_::binary-size(3), rest::binary>> = line
+    check_line(rest, hits + 1)
+  end
+
+  defp check_line(<<@xmas, _::binary>> = line, hits) do
+    <<_::binary-size(3), rest::binary>> = line
+    check_line(rest, hits + 1)
+  end
+
+  defp check_line(<<_::binary-size(1), rest::binary>>, hits), do: check_line(rest, hits)
+
+  # We include the new line in the count because it makes the rest of the stuff work better
+  defp line_length(<<@new_line, _::binary>>, count), do: count + 1
+  defp line_length(<<_::binary-size(1), rest::binary>>, count), do: line_length(rest, count + 1)
+
+  def column_count(grid, line_length) do
+    Enum.reduce(0..(line_length - 1), "", fn x, lines ->
+      columns({x, line_length - 2}, grid, line_length, lines)
+    end)
+    |> check_line(0)
+  end
+
+  def columns({_, y}, _, _, acc) when y < 0, do: <<acc::binary, @new_line>>
+
+  def columns({x, y}, grid, line_length, acc) do
+    char = :binary.part(grid, x + y * line_length, 1)
+    columns({x, y - 1}, grid, line_length, <<acc::binary, char::binary>>)
+  end
+
+  def south_east_diagonal(grid, line_length) do
+    se_diagonal_index({line_length - 2, 0}, line_length - 2, [])
+    |> Enum.reduce("", fn diagonal_indexes, acc ->
+      line =
+        diagonal_indexes
+        |> Enum.reduce("", fn {x, y}, acc ->
+          char = :binary.part(grid, x + y * line_length, 1)
+          <<acc::binary, char::binary>>
+        end)
+
+      <<acc::binary, line::binary, @new_line>>
+    end)
+    |> check_line(0)
+  end
+
+  # We've gone past bottom left.
+  def se_diagonal_index({0, y}, last_idx, acc) when y == last_idx, do: acc
+
+  # This is the first case hit - the top right
+  def se_diagonal_index({last_idx, 0}, last_idx, acc) do
+    se_diagonal_index({last_idx - 1, 0}, last_idx, [[{last_idx, 0}] | acc])
+  end
+
+  # This is the switch up case, where we round the corner on the top left hand side going down
+  def se_diagonal_index({x, _}, last_idx, acc) when x < 0 do
+    se_diagonal_index({0, 1}, last_idx, acc)
+  end
+
+  # this is going along the top row, we heading backwards on the x axis
+  def se_diagonal_index({x, 0} = current_cell, last_idx, acc) do
+    diagonal = [
+      current_cell | Enum.map(1..(last_idx - x), fn y_coord -> {x + 1 * y_coord, y_coord} end)
+    ]
+
+    se_diagonal_index({x - 1, 0}, last_idx, [diagonal | acc])
+  end
+
+  # this is going down the leftmost column.
+  def se_diagonal_index({0, y} = current, last_idx, acc) do
+    diagonal = [current | Enum.map(1..(last_idx - y), fn y_coord -> {y_coord, y + y_coord} end)]
+    se_diagonal_index({0, y + 1}, last_idx, [diagonal | acc])
+  end
+
+  def north_east_diagonal(grid, line_length) do
+    # It's - 2, 1 because of the newline char at the end of each line 1 because of the 0 index
+    # We start at X of 2 because first few rows can never match as they are too short.
+    ne_diagonal_idx({0, 0}, line_length - 2, [])
+    |> Enum.reduce("", fn diagonal_indexes, acc ->
+      line =
+        diagonal_indexes
+        |> Enum.reduce("", fn {x, y}, acc ->
+          char = :binary.part(grid, x + y * line_length, 1)
+          <<acc::binary, char::binary>>
+        end)
+
+      <<acc::binary, line::binary, @new_line>>
+    end)
+    |> check_line(0)
+  end
+
+  def ne_diagonal_idx({last_idx, y}, last_idx, acc) when y >= last_idx, do: acc
+
+  def ne_diagonal_idx({0, 0}, last_idx, acc) do
+    ne_diagonal_idx({1, 0}, last_idx, [[{0, 0}] | acc])
+  end
+
+  def ne_diagonal_idx({x, 0}, last_idx, acc) when x > last_idx do
+    ne_diagonal_idx({x - 1, 1}, last_idx, acc)
+  end
+
+  def ne_diagonal_idx({x, 0} = current_cell, last_idx, acc) do
+    diagonal = [current_cell | Enum.map(1..x, fn y_coord -> {x - 1 * y_coord, y_coord} end)]
+    ne_diagonal_idx({x + 1, 0}, last_idx, [diagonal | acc])
+  end
+
+  def ne_diagonal_idx({x, y} = current, last_idx, acc) do
+    diagonal = [
+      current | Enum.map(1..(last_idx - y), fn y_coord -> {x - 1 * y_coord, y + y_coord} end)
+    ]
+
+    ne_diagonal_idx({x, y + 1}, last_idx, [diagonal | acc])
+  end
 end
